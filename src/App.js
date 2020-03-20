e4e27f3ef64883aea1163f4855e900d85b90972d
yuqi <yuqi.yang@mail.mcgill.ca>
2020-03-19 22:04:15 -0400

    home page set up and install dayjs time format package

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+//component
+import Navbar from "./components/Navbar";
+
+//pages
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#33c9dc",
+      main: "#00bcd4",
+      dark: "#008394",
+      contrastText: "#fff"
+    }
+  },
+
+  secondary: {
+    light: "#ff6333",
+    main: "#ff3d00",
+    dark: "#b22a00",
+    constrastText: "#fff"
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
+
+function App() {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
+  );
+}
+
+export default App;
