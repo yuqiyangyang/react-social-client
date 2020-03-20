35346df03d0b15ebc2492b1696103f093df22dc1
yuqi <yuqi.yang@mail.mcgill.ca>
2020-03-20 17:20:44 -0400

    add signup page

--- src/App.js
+++ src/App.js
@@ -5,32 +5,30 @@ import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 //component
 import Navbar from "./components/Navbar";
+import themeFile from "./util/theme";
+import jwtDecode from "jwt-decode";
+import AuthRoute from "./util/AuthRoute";
 
 //pages
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: "#33c9dc",
-      main: "#00bcd4",
-      dark: "#008394",
-      contrastText: "#fff"
-    }
-  },
+const theme = createMuiTheme(themeFile);
 
-  secondary: {
-    light: "#ff6333",
-    main: "#ff3d00",
-    dark: "#b22a00",
-    constrastText: "#fff"
-  },
-  typography: {
-    useNextVariants: true
+//decode token npm install --save jwt-decode
+const token = localStorage.FBIdToken;
+
+let authenticated;
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = "/login";
+    authenticated = false;
+  } else {
+    authenticated = true;
   }
-});
+}
 
 function App() {
   return (
@@ -41,8 +39,18 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={home} />
-              <Route exact path="/login" component={login} />
-              <Route exact path="/signup" component={signup} />
+              <AuthRoute
+                exact
+                path="/login"
+                component={login}
+                authenticated={authenticated}
+              />
+              <AuthRoute
+                exact
+                path="/signup"
+                component={signup}
+                authenticated={authenticated}
+              />
             </Switch>
           </div>
         </Router>
--- src/pages/login.js
+++ src/pages/login.js
@@ -11,32 +11,9 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
-const styles = {
-  form: {
-    textAlign: "center"
-  },
-  image: {
-    margin: "20px auto 20px auto"
-  },
-  pageTitle: {
-    margin: "10px auto 20px auto"
-  },
-  textField: {
-    margin: "10px auto 20px auto"
-  },
-  button: {
-    marginTop: 20,
-    position: "relative"
-  },
-  customError: {
-    color: "red",
-    fontSize: "0.8rem",
-    marginTop: 10
-  },
-  progress: {
-    position: "absolute"
-  }
-};
+const styles = theme => ({
+  ...theme.spreadThis
+});
 
 class login extends Component {
   constructor() {
@@ -60,6 +37,8 @@ class login extends Component {
     axios
       .post("/login", userData)
       .then(res => {
+        console.log(res.data);
+        localStorage.setItem("FBIdToken", `Bearer ${res.data.token}`);
         this.setState({
           loading: false
         });
