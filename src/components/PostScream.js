97a979e04322c6d47740a27eee9b125930c6a794
yuqi <yuqi.yang@mail.mcgill.ca>
2020-03-25 17:14:43 -0400

    add CLEAR_ERRORS on PostScream

--- src/components/PostScream.js
+++ src/components/PostScream.js
@@ -13,7 +13,7 @@ import AddIcon from "@material-ui/icons/Add";
 import CloseIcon from "@material-ui/icons/Close";
 // Redux stuff
 import { connect } from "react-redux";
-import { postScream } from "../redux/actions/dataActions";
+import { postScream, clearErrors } from "../redux/actions/dataActions";
 
 const styles = theme => ({
   ...theme.spreadThis,
@@ -38,6 +38,7 @@ class PostScream extends Component {
     body: "",
     errors: {}
   };
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.UI.errors) {
       this.setState({
@@ -52,6 +53,7 @@ class PostScream extends Component {
     this.setState({ open: true });
   };
   handleClose = () => {
+    this.props.clearErrors();
     this.setState({ open: false });
   };
   handleChange = event => {
@@ -126,6 +128,7 @@ class PostScream extends Component {
 
 PostScream.propTypes = {
   postScream: PropTypes.func.isRequired,
+  clearErrors: PropTypes.func.isRequired,
   UI: PropTypes.object.isRequired
 };
 
@@ -133,6 +136,6 @@ const mapStateToProps = state => ({
   UI: state.UI
 });
 
-export default connect(mapStateToProps, { postScream })(
+export default connect(mapStateToProps, { postScream, clearErrors })(
   withStyles(styles)(PostScream)
 );
