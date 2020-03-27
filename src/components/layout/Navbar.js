aada603b63891496f6ac3c4af67ac46a864f71d1
yuqi <yuqi.yang@mail.mcgill.ca>
2020-03-27 14:37:30 -0400

    notifications

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import MyButton from "../../util/MyButton";
 import PostScream from "../scream/PostScream";
+import Notifications from "./Notifications";
 
 //MUI stuff
 import AppBar from "@material-ui/core/AppBar";
@@ -12,7 +13,6 @@ import Button from "@material-ui/core/Button";
 
 //Icons
 import HomeIcon from "@material-ui/icons/Home";
-import Notifications from "@material-ui/icons/Notifications";
 
 export class Navbar extends Component {
   render() {
@@ -29,9 +29,8 @@ export class Navbar extends Component {
                   <HomeIcon />
                 </MyButton>
               </Link>
-              <MyButton tip="Notifications">
-                <Notifications />
-              </MyButton>
+
+              <Notifications />
             </Fragment>
           ) : (
             <Fragment>
--- src/components/scream/ScreamDialog.js
+++ src/components/scream/ScreamDialog.js
@@ -49,7 +49,9 @@ const styles = theme => ({
 
 class ScreamDialog extends Component {
   state = {
-    open: false
+    open: false,
+    oldPath: "",
+    newPath: ""
   };
 
   componentDidMount() {
@@ -59,11 +61,21 @@ class ScreamDialog extends Component {
   }
 
   handleOpen = () => {
-    this.setState({ open: true });
+    let oldPath = window.location.pathname;
+
+    const { userHandle, screamId } = this.props;
+    const newPath = `/users/${userHandle}/scream/${screamId}`;
+
+    if (oldPath === newPath) oldPath = `/users/${userHandle}`;
+
+    window.history.pushState(null, null, newPath);
+
+    this.setState({ open: true, oldPath, newPath });
     this.props.getScream(this.props.screamId);
   };
 
   handleClose = () => {
+    window.history.pushState(null, null, this.state.oldPath);
     this.setState({ open: false });
     this.props.clearErrors();
   };
